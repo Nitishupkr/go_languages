0c8d1e7223266781b1b57236bce045c55b86a4d0
Nitishupkr <nitishupkr>
2022-10-14 03:13:31 +0530

    First commit

---
+++ Arrays.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s [2]string
+
+	s[0] = "Hello"
+	s[1] = "world"
+	fmt.Println(s[0], s[1])
+	fmt.Println(s)
+	finonacci := [12]int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89}
+	fmt.Println(finonacci)
+
+}
